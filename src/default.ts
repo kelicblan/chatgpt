aa1f8df220abe1bbdf1a3e7096633cdd15dbb583
kelicblan <ke@linchunbiao.cn>
2023-03-24 08:50:56 +0800

    Add files via upload

---
+++ src/default.ts
@@ -0,0 +1,14 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 本站由 [智慧的格子衫](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg3NzYwMTQ0OA==&scene=124#wechat_redirect) 搭建。
+- 想要部署的朋友，可关注公众号：[智慧的格子衫](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg3NzYwMTQ0OA==&scene=124#wechat_redirect)，添加我个人微信获取保姆级部署文档。
+- 关注公众号：[智慧的格子衫](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzg3NzYwMTQ0OA==&scene=124#wechat_redirect) ，回复 **ApiKey** 获取最新的ApiKey。
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
